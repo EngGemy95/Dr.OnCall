9ec930d8251535c08a3198248490a105b6c527cb
EngGemy95 <mohamed.gamalfayed@gmail.com>
2024-03-10 14:58:33 +0200

    update register bloc to handle new response

--- lib/presentation/bloc/register/register_bloc.dart
+++ lib/presentation/bloc/register/register_bloc.dart
@@ -13,14 +13,17 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   RegisterBloc({required this.registerUseCase}) : super(RegisterInitial()) {
     on<RegisterEvent>((event, emit) async {
       if (event is SignUpEvent) {
-          emit(RegisterLoadingState());
-          final response = await registerUseCase(event.registerRequest);
-          response.fold((failure) {
-            emit(RegisterErrorState(message: failure.message));
-          }, (registerModel) {
+        emit(RegisterLoadingState());
+        final response = await registerUseCase(event.registerRequest);
+        response.fold((failure) {
+          emit(RegisterErrorState(message: failure.message));
+        }, (registerModel) {
+          if (registerModel.code == 200) {
             emit(RegisterSuccessState());
-          });
-       
+          } else {
+            emit(RegisterErrorState(message: registerModel.message));
+          }
+        });
       }
     });
   }
